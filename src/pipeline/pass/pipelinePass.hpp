37cc075c82d3a0596807b701b2c2b519056e12f0
EngineersBox <toastcraft.info@gmail.com>
2022-05-17 18:27:08 +1000

    Created skeleton classes for pipeline and passes

---
+++ src/pipeline/pass/pipelinePass.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifndef GL_COLOUR_GRADING_PIPELINEPASS_HPP
+#define GL_COLOUR_GRADING_PIPELINEPASS_HPP
+
+namespace GLCG::Pipelines {
+    class PipelinePass {
+
+    };
+}
+
+#endif //GL_COLOUR_GRADING_PIPELINEPASS_HPP
---
+++ src/pipeline/pipeline.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifndef GL_COLOUR_GRADING_PIPELINE_HPP
+#define GL_COLOUR_GRADING_PIPELINE_HPP
+
+namespace GLCG::Pipelines {
+    class Pipeline {
+
+    };
+}
+
+#endif //GL_COLOUR_GRADING_PIPELINE_HPP
